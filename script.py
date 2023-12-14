1976c143c8fa064d08e45e5e0b467285d09bc175
Alexey S <lexx@42point.com>
2023-12-14 19:03:25 +0300

    first attempt

---
+++ script.py
@@ -0,0 +1,45 @@
+import json
+import requests
+from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+
+# Load the service account key
+key_path = './service_account.json'
+credentials = service_account.Credentials.from_service_account_file(
+	key_path,
+	scopes=['https://www.googleapis.com/auth/indexing'],
+)
+
+# Refresh the credentials if necessary
+if credentials.expired:
+	credentials.refresh(Request())
+
+# Read the batch of URLs from a file
+with open('urls.txt', 'r') as file:
+	batch = file.read().splitlines()
+
+# Prepare the batch request
+items = []
+for line in batch:
+	item = {
+		'Content-Type': 'application/http',
+		'Content-ID': '',
+		'body': 'POST /v3/urlNotifications:publish HTTP/1.1\n'
+				'Content-Type: application/json\n\n' +
+				json.dumps({
+					'url': line,
+					'type': 'URL_UPDATED'
+				})
+	}
+	items.append(item)
+
+# Configure the request
+headers = {
+	'Authorization': 'Bearer ' + credentials.token,
+	'Content-Type': 'multipart/mixed'
+}
+data = {'multipart': items}
+response = requests.post('https://indexing.googleapis.com/batch', headers=headers, json=data)
+
+# Print the response
+print(response.text)
